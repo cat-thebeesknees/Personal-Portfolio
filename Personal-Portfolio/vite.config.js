1bbd7c681931531106c9429a07f348d4e313270a
Catherine Hilton <126924004+cat-thebeesknees@users.noreply.github.com>
2025-06-17 21:21:07 -0400

    Ready to deploy

--- Personal-Portfolio/vite.config.js
+++ Personal-Portfolio/vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/Personal-Portfolio/",
   plugins: [react()],
   assetsInclude: ["**/*.docx"],
   build: {
     outDir: "dist",
   },
+  base: "/Personal-Portfolio/",
 });
